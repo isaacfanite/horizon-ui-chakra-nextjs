5342e49e8d053ff31737fc97adadce460ee03cd0
isaacngabwali <135863652+isaacotim101@users.noreply.github.com>
2024-08-07 19:45:45 +0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const userDetails = localStorage.getItem('userDetails');
-      
-      if (userDetails) {
-        router.push('/admin/default');
-      } else {
-        router.push('/login');
-      }
-    }
-  }, [router]);
-
+import { redirect } from 'next/navigation';
+export default function Home({}) {
+  redirect('/admin/default');
 }
